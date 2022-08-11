5c3c79828fd86d4840b36092ae4e90ecec23e9a1
Lazaro Moreno <lazmoreno29@gmail.com>
2022-08-10 23:17:44 -0400

    first commit

---
+++ src/game.js
@@ -0,0 +1,15 @@
+//console.log("Hello World")
+const cards = [
+  {name: "Ace", suite: "♠️", value: 1 || 11, img:""},
+  {name: "King", suite: "♠️", value: 10, img:""},
+  {name: "Queen", suite: "♠️", value: 10, img:""},
+  {name: "Jack", suite: "♠️", value: 10, img:""}
+
+];
+
+console.log(cards);
+class blackJackGame {
+  constructor(cards) {
+  }
+  
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// const cards = [
+//   {name: "Ace", suite: "♠️", value: 1 || 11, img:""},
+//   {name: "King", suite: "♠️", value: 10, img:""},
+//   {name: "Queen", suite: "♠️", value: 10, img:""},
+//   {name: "Jack", suite: "♠️", value: 10, img:""}
+
+// ];
