b089dfe6241f473f6ec7721c0e0271f20973eca2
Lazaro Moreno <lazmoreno29@gmail.com>
2022-08-15 23:33:50 -0400

    html and css work

--- src/card.js
+++ src/card.js
@@ -1,8 +1,8 @@
 class Card {
   constructor(suite, value) {
     this.suite = suite;
-    this.value = value
-    this.cardImage = ''
+    this.value = value;
+    this.cardImage = "";
     this.score = 0;
     this.createCard();
   }
@@ -13,6 +13,12 @@ class Card {
   }
 
   addScore() {
-    this.score = !['J', 'Q', 'K', 'A'].includes(this.value) ? Number(this.value) : !['J', 'Q', 'K'].includes(this.value) ? 10  : 11
+    this.score = !["J", "Q", "K", "A"].includes(this.value)
+      ? Number(this.value)
+      : !["J", "Q", "K"].includes(this.value)
+      ? 11
+      : !["A"].includes(this.value)
+      ? 10
+      : 11;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-// const newGame = new Game('table one', ['Dealer', 'Name1'], 1);
+const newGame = new Game("table one", ["Dealer", "Name1"], 1);
 
-// console.log({ newGame, players: newGame.players, deck: newGame.players[0].cardsInTotalDeck, deckLength: newGame.players[0].cardsInTotalDeck.length })
+console.log({
+  newGame,
+  players: newGame.players,
+  deck: newGame.players[0].cardsInTotalDeck,
+  deckLength: newGame.players[0].cardsInTotalDeck.length,
+});
+
+let dealersHand =  document.querySelectorAll( "#dealerscard  img")
+console.log(dealersHand);
